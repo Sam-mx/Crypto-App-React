1ac7c2a5a63906df89d79185827bd212e71c84fb
San Shwe <sanxhwe@gmail.com>
2024-03-28 22:56:40 +0630

    Coin Component

--- src/component/Coin/Coin.js
+++ src/component/Coin/Coin.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import './Coin.css'
 
-const Coin = () => {
+const Coin = ({image,name,price,volume,priceChange,marketCap}) => {
   return (
-    <div>Coin</div>
+    <div className='coin-container'>
+        <div className='coin-row'>
+            <div className='coin'>
+                <img src={image} alt='crypto'/>
+                <h1>{name}</h1>
+                <p className='coin-symbol'></p>
+            </div>
+
+            <div className='coin-data'>
+                <p className='coin-price'>${price}</p>
+                
+                {priceChange<0 ? (
+                    <p className='coin-percent red'>{priceChange.tofixed(2)}%</p>
+                    ) : (
+                    <p className='coin-percent green'>{priceChange.tofixed(2)}%</p>
+                )}
+
+                <p className='coin-marketcap'>
+                    Mkt Cap: ${marketCap.toLocaleString()}
+                </p>
+            </div>
+
+        </div>
+    </div>
   )
 }
 
